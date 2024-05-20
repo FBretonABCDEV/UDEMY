7b0e766072635dd6ecd611f9c4c68466ced52a84
FBretonABCDEV <fred.eks@orange.fr>
2024-05-20 15:15:10 +0200

    premier programme

---
+++ C#/Premier_programme/Premier_programme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Premier_programme
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            /* VARIABLES */
+
+            String nom;
+            int age = 0;
+
+            /* DEBUT PROGRAMME */
+
+            Console.WriteLine("Quel est ton nom ?");
+            nom = Console.ReadLine();
+            while(age <= 0)
+            {
+                try
+                {
+                    Console.WriteLine("Quel ton âge ?");
+                    age = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Bonjour, vous vous appelez " + nom + ", vous avez " + age + " ans.");
+                    age += 1;
+                    Console.WriteLine("Bientôt vous aurez " + age + " ans");
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur. Vous devez entrer un âge valide.");
+                }
+            }
+            
+            /* FIN PROGRAMME */
+        }
+    }
+}
