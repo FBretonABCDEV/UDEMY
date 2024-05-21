c9057bd2157206f9ae4dbd3330fb9492807ea502
FBretonABCDEV <fred.eks@orange.fr>
2024-05-21 18:58:16 +0200

    projet Jeux de maths

--- C#/Jeux_de_maths/Jeux_de_maths/Program.cs
+++ C#/Jeux_de_maths/Jeux_de_maths/Program.cs
@@ -9,15 +9,16 @@ namespace Jeux_de_maths
 
             /* VARIABLES */
 
-            int nb1 = 0, nb2 = 0, result = 0;
-            String operateur = " + ";
+            int nb1 = 0, nb2 = 0, result = 0, reponseValide = 0; ;
+            String operateur = "";
 
             /* DEBUT PROGRAMME */
 
-            //Afficher la question
+            //Afficher la question 1
             Console.WriteLine("Question n°1/5 :");
 
             //cas 1 operateur -> +
+            operateur = " + ";
             nb1 = 8; nb2 = 4; result = 0;
             result = nb1 + nb2;
 
@@ -25,14 +26,42 @@ namespace Jeux_de_maths
             Console.Write(nb1 + operateur + nb2 +" = ");
             int nbUtilisateur = int.Parse(Console.ReadLine());
 
+            //Valider la réponse
             if(nbUtilisateur == result)
             {
                 Console.WriteLine("Bonne réponse");
+                reponseValide += 1;
             }
             else
             {
                 Console.WriteLine("Mauvaise réponse.");
             }
+
+            //Afficher la question 2
+            Console.WriteLine("Question n°2/5 :");
+
+            //cas 2 operateur -> -
+            operateur = " - ";
+            nb1 = 8; nb2 = 4; result = 0;
+            result = nb1 - nb2;
+
+            //Demander résultat
+            Console.Write(nb1 + operateur + nb2 + " = ");
+            nbUtilisateur = int.Parse(Console.ReadLine());
+
+            //Valider la réponse
+            if (nbUtilisateur == result)
+            {
+                Console.WriteLine("Bonne réponse");
+                reponseValide += 1;
+            }
+            else
+            {
+                Console.WriteLine("Mauvaise réponse.");
+            }
+
+            //Afficher nombre bonnes réponses
+            Console.WriteLine("nombre de bonne(s) réponse(s) : "+reponseValide);
 
             /* FIN PROGRAMME */
         }
--- C#/Jeux_de_maths/Jeux_de_maths/obj/Debug/net6.0/Jeux_de_maths.AssemblyInfo.cs
+++ C#/Jeux_de_maths/Jeux_de_maths/obj/Debug/net6.0/Jeux_de_maths.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jeux_de_maths")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466cf5e34bfec90b698865a24d0141dc885956f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1820ba3bcc528ca6bfdada9dd5c99be35fbae5a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jeux_de_maths")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jeux_de_maths")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
