4167b07c98d9ae7e0b50f69b7c045720b1668211
FBretonABCDEV <fred.eks@orange.fr>
2024-05-21 21:12:06 +0200

    codage fonctions

--- C#/Jeux_de_maths/Jeux_de_maths/Program.cs
+++ C#/Jeux_de_maths/Jeux_de_maths/Program.cs
@@ -4,44 +4,114 @@ namespace Jeux_de_maths
 {
     class Program
     {
+        /* FONCTIONS */
+
+        static int GenererOperation()
+        {
+            Random nbAleatoire = new Random();
+
+            string operateur = " + ";
+            int nb1 = nbAleatoire.Next(1, 10);
+            int nb2 = nbAleatoire.Next(1, 10);
+            int op = nbAleatoire.Next(4);
+            int result = 0;
+
+            switch (op)
+            {
+                case 0:
+                    result = nb1 + nb2;
+                    operateur = " + ";
+                    break;
+                case 1:
+                    result = nb1 - nb2;
+                    operateur = " - ";
+                    break;
+                case 2:
+                    result = nb1 * nb2;
+                    operateur = " * ";
+                    break;
+                case 3:
+                    result = nb1 / nb2;
+                    operateur = " / ";
+                    break;
+                default:
+                    break;
+            }
+            Console.Write(nb1 + operateur + nb2 + " = ");
+            return result;
+        }
+        static int DemanderResultat()
+        {
+            int nbUtilisateur = 0;
+            bool nbValide = false;
+            while (!nbValide)
+            {
+                try
+                {
+                    nbUtilisateur = int.Parse(Console.ReadLine());
+                    nbValide = true;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Vous devez entrer un nombre.\n");
+                }
+            }
+            
+            return nbUtilisateur;
+        }
+        
+
+        /* FONCTIONS PRINCIPALE */
         public static void Main(String[] args)
         {
-
-            /* VARIABLES */
+            /* CONSTRANTES */
 
             const int QUESTIONS_MAX = 5;
 
-            int nb1 = 0, nb2 = 0, result = 0, reponseValide = 0, numQuestion = 0;
-            String operateur = "";
+            /* VARIABLES */
+
+            int reponseJoueur = 0, result = 0,  nbPoints = 0, numQuestion = 0;
 
             /* DEBUT PROGRAMME */
 
-            for(numQuestion = 1; numQuestion <= QUESTIONS_MAX; numQuestion++)
+            for (numQuestion = 1; numQuestion <= QUESTIONS_MAX; numQuestion++)
             {
-                //Afficher la question
+                //Afficher le numéro de question
                 Console.WriteLine("Question n°" + numQuestion + "/" + QUESTIONS_MAX + " :");
 
-                //cas 1 operateur -> +
-                operateur = " + ";
-                nb1 = 8; nb2 = 4; result = 0;
-                result = nb1 + nb2;
 
-                //Demander résultat
-                Console.Write(nb1 + operateur + nb2 + " = ");
-                int nbUtilisateur = int.Parse(Console.ReadLine());
+                //Générer et afficher opération
+                result = GenererOperation();
 
-                //Valider la réponse
-                if (nbUtilisateur == result)
+                //Demander un résultat au joueur
+                reponseJoueur = DemanderResultat();
+
+                //Calculer nombre de points
+                if (reponseJoueur == result)
                 {
                     Console.WriteLine("Bonne réponse.\n");
-                    reponseValide += 1;
+                    nbPoints += 1;
                 }
                 else
                 {
                     Console.WriteLine("Mauvaise réponse.\n");
                 }
             }
-            Console.WriteLine("Nombre de points : "+reponseValide+" / "+QUESTIONS_MAX);
+
+            //Afficher le score
+            Console.WriteLine("Nombre de points : "+nbPoints+" / "+QUESTIONS_MAX);
+            if(nbPoints == QUESTIONS_MAX / 2)
+            {
+                Console.WriteLine("Pas mal");
+            }
+            if (nbPoints < QUESTIONS_MAX / 2)
+            {
+                Console.WriteLine("Essayer de faire mieux la prochaine.");
+            }
+            if (nbPoints > QUESTIONS_MAX / 2)
+            {
+                Console.WriteLine("Excellent");
+            }
 
             /* FIN PROGRAMME */
         }
--- C#/Jeux_de_maths/Jeux_de_maths/obj/Debug/net6.0/Jeux_de_maths.AssemblyInfo.cs
+++ C#/Jeux_de_maths/Jeux_de_maths/obj/Debug/net6.0/Jeux_de_maths.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jeux_de_maths")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9057bd2157206f9ae4dbd3330fb9492807ea502")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c63b5153e44636f6e798d875a21dc39e961819f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jeux_de_maths")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jeux_de_maths")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
