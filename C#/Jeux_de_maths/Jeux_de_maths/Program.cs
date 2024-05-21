1820ba3bcc528ca6bfdada9dd5c99be35fbae5a3
FBretonABCDEV <fred.eks@orange.fr>
2024-05-21 18:48:34 +0200

    projet Jeux de maths

---
+++ C#/Jeux_de_maths/Jeux_de_maths/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Jeux_de_maths
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            /* VARIABLES */
+
+            int nb1 = 0, nb2 = 0, result = 0;
+            String operateur = " + ";
+
+            /* DEBUT PROGRAMME */
+
+            //Afficher la question
+            Console.WriteLine("Question n°1/5 :");
+
+            //cas 1 operateur -> +
+            nb1 = 8; nb2 = 4; result = 0;
+            result = nb1 + nb2;
+
+            //Demander résultat
+            Console.Write(nb1 + operateur + nb2 +" = ");
+            int nbUtilisateur = int.Parse(Console.ReadLine());
+
+            if(nbUtilisateur == result)
+            {
+                Console.WriteLine("Bonne réponse");
+            }
+            else
+            {
+                Console.WriteLine("Mauvaise réponse.");
+            }
+
+            /* FIN PROGRAMME */
+        }
+    }
+}
+
