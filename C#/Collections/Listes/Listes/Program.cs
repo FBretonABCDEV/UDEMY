134bde823ee8b1ffd25e84105bddb41ec07d58d0
FBretonABCDEV <fred.eks@orange.fr>
2024-05-23 21:30:02 +0200

    exercices listes

--- C#/Collections/Listes/Listes/Program.cs
+++ C#/Collections/Listes/Listes/Program.cs
@@ -9,7 +9,7 @@ namespace Listes
 
         static void liste()
         {
-            List<string> listeNoms = new List<string>();
+            /*List<string> listeNoms = new List<string>();
 
             while (true)
             {
@@ -22,19 +22,25 @@ namespace Listes
                 listeNoms.Add(nom);
             }
             //Filtrer : Supprimer les noms qui terminent par le caractère 'e'
-            for (int i = 0; i < listeNoms.Count; i++)
+            for (int i = listeNoms.Count - 1; i >=0; i--)
             {
                 if (listeNoms[i][listeNoms[i].Length-1] == 'e')
                 {
                     listeNoms.RemoveAt(i);
                 }
             }
-            AfficherListe(listeNoms, true);
+            AfficherListe(listeNoms, true);*/
+            //Afficher les noms en communs des 2 listes
+            List<string> liste1 = new List<string>() { "Paul", "Marc", "Emilie", "Edouard", "Luc", "Sophie" };
+            List<string> liste2 = new List<string>() { "Paul", "Quentin", "Emilie", "Francis", "Luc", "Jean" };
+            AfficherElementCommun(liste1, liste2);
+
+
         }
-        static void AfficherListe(List<string> _liste, bool ordreDescendant)
+        static void AfficherListe(List<string> _liste, bool _ordreDescendant)
         {
             Console.WriteLine("\nLaliste des noms est : \n");
-            if(!ordreDescendant)
+            if(!_ordreDescendant)
             {
                 for (int i = 0; i < _liste.Count; i++)
                 {
@@ -49,7 +55,7 @@ namespace Listes
                 }
             }
             //Afficher le nom le plus grand
-            string nomPlusGrand = _liste[0];
+            /*string nomPlusGrand = _liste[0];
             for (int i = 0; i < _liste.Count; i++)
             {
                 if(nomPlusGrand.Length < _liste[i].Length)
@@ -57,7 +63,19 @@ namespace Listes
                     nomPlusGrand = _liste[i];
                 }
             }
-            Console.WriteLine("\nLe nom le plus grand de la liste est : " + nomPlusGrand);
+            Console.WriteLine("\nLe nom le plus grand de la liste est : " + nomPlusGrand);*/
+        }
+
+        static void AfficherElementCommun(List<string> _liste1, List<string> _liste2)
+        {
+            List<string> liste3 = new List<string>();
+            for (int i = 0; i < _liste1.Count; i++)
+            {
+                if (_liste2.Contains(_liste1[i]))
+                {
+                    Console.WriteLine(_liste1[i]);
+                }
+            }
         }
 
         /* FONCTION PRINCIPALE */
