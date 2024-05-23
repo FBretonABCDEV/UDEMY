a066f7ae26bddb411bd17a18ec5bb7c071ef87f8
FBretonABCDEV <fred.eks@orange.fr>
2024-05-23 16:59:47 +0200

    exercices tableaux

--- C#/Collections/Collections/Program.cs
+++ C#/Collections/Collections/Program.cs
@@ -16,9 +16,20 @@ namespace Collection
         /* FONCTION PRINCIPALE */
         public static void Main(string[] args)
         {
-            int[] monTableau = new int[] {200, 40, 15, 8, 12};
+            //int[] monTableau = new int[] { 200, 40, 15, 8, 12 };
             
-            AfficherTableau(monTableau);
+            const int TAILLE_TABLEAU = 12;
+            
+            int[] t = new int[TAILLE_TABLEAU];
+
+            //Initialisation tableau valeurs aléatoires 0 à 100
+            Random aleatoire = new Random();
+            for (int i = 0; i < t.Length; i++)
+            {
+                int nombre = aleatoire.Next(101);
+                t[i] = nombre;
+            }
+            AfficherTableau(t);
         }
     }
 }
