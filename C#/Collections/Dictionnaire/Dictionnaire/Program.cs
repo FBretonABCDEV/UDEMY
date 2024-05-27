bf162b5b296adea6a9a3c9eedc5ba214cb0928b6
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-05-27 13:51:05 +0200

    Le dictionnaire

---
+++ C#/Collections/Dictionnaire/Dictionnaire/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dictionnaire
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Dictionary
+            //Clef -> valeur
+            //nom numéro
+            var dictionnaire = new Dictionary<string, string>();
+
+            //Méthode 1 pour ajouter valeur
+            dictionnaire.Add("Jean", "+33628153467");
+            dictionnaire.Add("Henry", "+33652439481");
+
+            //Méthode 2 pour ajouter valeur
+            dictionnaire["Marie"] = "+33665483518";
+            Console.WriteLine(dictionnaire["Marie"]);
+        }
+    }
+}
