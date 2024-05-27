6616e533f9b8697d3632f7a424f25df53468f3fa
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-05-27 14:47:18 +0200

    La boucle foreach

---
+++ C#/Collections/BoucleForEach/BoucleForEach/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BoucleForEach
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //foreach tableau
+            string[] noms = new string[] {"Raymond", "Sarah", "Kate"};
+
+            //foreach parcours la collection
+            Console.WriteLine("[Tableau]");
+            foreach (var nom in noms)
+            {
+                Console.WriteLine(nom);
+            }
+            Console.WriteLine();
+
+            //foreach list
+            string[] listeNoms = new string[] { "Laura", "Francis", "Justine", "Martin" };
+            Console.WriteLine("[Liste]");
+            foreach (var nom in listeNoms)
+            {
+                Console.WriteLine(nom);
+            }
+            Console.WriteLine();
+
+            var dictionnaire = new Dictionary<string, string>();
+
+            dictionnaire.Add("Jean", "+33628153467");
+            dictionnaire.Add("Henry", "+33652439481");
+            dictionnaire.Add("Marie", "+33665483518");
+            dictionnaire.Add("Edouard", "+33656841835");
+            Console.WriteLine("[Dictionnaire]");
+            foreach (var element in dictionnaire)
+            {
+                Console.WriteLine("Clef : "+element.Key+" - Valeur : "+element.Value);
+            }
+        }
+    }
+}
