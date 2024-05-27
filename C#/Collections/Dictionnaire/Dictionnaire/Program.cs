16647a793d0a6877bf3b5758b68811bb76cbe529
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-05-27 14:12:49 +0200

    Le dictionnaire

--- C#/Collections/Dictionnaire/Dictionnaire/Program.cs
+++ C#/Collections/Dictionnaire/Dictionnaire/Program.cs
@@ -9,7 +9,10 @@ namespace Dictionnaire
             //Dictionary
             //Clef -> valeur
             //nom numéro
+            //Pas besoin de boucle pour chercher un numéro
+
             var dictionnaire = new Dictionary<string, string>();
+            string personneAChercher = "Edouard";
 
             //Méthode 1 pour ajouter valeur
             dictionnaire.Add("Jean", "+33628153467");
@@ -17,7 +20,19 @@ namespace Dictionnaire
 
             //Méthode 2 pour ajouter valeur
             dictionnaire["Marie"] = "+33665483518";
+            dictionnaire["Edouard"] = "+33656841835";
+
             Console.WriteLine(dictionnaire["Marie"]);
+
+            //Gestion erreur de clef ou affichage de la valeur si la clef existe
+            if (dictionnaire.ContainsKey(personneAChercher))
+            {
+                Console.WriteLine(dictionnaire[personneAChercher]);
+            }
+            else
+            {
+                Console.WriteLine("Cette personne n'est pas dans le dictionnaire.");
+            }
         }
     }
 }
