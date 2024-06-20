07145c6dd14d2c9bf33c43ddaeb63bb8451b512b
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-06-20 18:34:22 +0200

    premier programme poo CSharp Variable de class

--- C#/Programme_poo/Programme_poo/Program.cs
+++ C#/Programme_poo/Programme_poo/Program.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace programme_poo
 {
     class Personne
     {
-        string nom;
-        int age;
-        string emploi;
+        //Variable de class
+        public static int nombrePersonnes;//static variable commune à toutes les instances
 
+        //variable d'instance
+        public string nom;
+        private int age;
+        private string emploi;
+        private int numeroPersonne = 0;
+
+        //Constructeur
         public Personne(string _nom, int _age, string _emploi)
         {
             this.nom = _nom;
             this.age = _age;
             this.emploi = _emploi;
+
+            nombrePersonnes++;
+
+            this.numeroPersonne = nombrePersonnes;
         }
 
         public void Afficher()
         {
+            Console.WriteLine("Personne N° " + numeroPersonne);
             Console.WriteLine("NOM : " + nom);
             Console.WriteLine("  AGE : " + age + " ans");
             Console.WriteLine("  EMPLOI : " + emploi);
@@ -44,26 +56,31 @@ namespace programme_poo
             for (int i = 0; i < noms.Count; i++)
             {
                 AfficherInfosPersonne(noms[i], ages[i], emplois[i]);
-            }*/
+            }
 
             Personne personne1 = new Personne("Paul", 30, "Développeur");
             personne1.Afficher();
 
             Personne personne2 = new Personne("Jacques", 35, "Professeur");
-            personne2.Afficher();
+            personne2.Afficher();*/
 
             //liste de Class Personne
             List<Personne> listeCivile = new List<Personne>();
             listeCivile.Add(new Personne("Edouard", 28, "Mécanicien"));
             listeCivile.Add(new Personne("Nathalie", 42, "Secrétaire"));
-            listeCivile.Add(new Personne("Pauline", 36, "Médecin"));
-            listeCivile.Add(new Personne("jean", 36, "Plombier"));
+            listeCivile.Add(new Personne("Pauline", 18, "Etudiant"));
+            listeCivile.Add(new Personne("Nicolas", 8, "CP"));
+
+            //Trier par nom
+            //listeCivile = listeCivile.OrderBy(p => p.nom).ToList();
 
             //Afficher chaque personne de la liste
             foreach(Personne element in listeCivile)
             {
                 element.Afficher();
+                Console.WriteLine();
             }
+            Console.WriteLine("Nombre total de personnes : " + Personne.nombrePersonnes);
         }
     }
 }
