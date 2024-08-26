9a2e2e4f4130c106e7bd08a000b7e69eec176c49
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-08-26 09:45:53 +0200

    Json serialiser deserialiser

---
+++ C#/programme_json/programme_json/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace programme_json
+{
+    class Personne
+    {
+        public string nom;
+        public int age;
+        public bool majeur;
+
+        public void Afficher()
+        {
+            Console.WriteLine(" - Nom : " + this.nom + " - Age : " + this.age + "ans");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne personne1 = new Personne();
+            personne1.nom = "toto";
+            personne1.age = 20;
+            personne1.majeur = true;
+
+            personne1.Afficher();
+
+            string jsonToto = JsonConvert.SerializeObject(personne1);
+            Console.WriteLine(jsonToto);
+
+            string jsonTiti = "{\"nom\":\"titi\",\"age\":15,\"majeur\":false}";
+            Personne titi  = JsonConvert.DeserializeObject<Personne>(jsonTiti);
+
+            titi.Afficher();
+        }
+    }
+}
