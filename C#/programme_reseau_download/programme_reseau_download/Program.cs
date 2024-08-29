59721f50ec32d36d060b4c3164687dccbe0d140a
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-08-29 10:49:29 +0200

    téléchargement synchrone image

---
+++ C#/programme_reseau_download/programme_reseau_download/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+
+namespace programme_reseau_download
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://codeavecjonathan.com/res/papillon.jpg";
+
+            var webClient = new WebClient();
+
+            Console.WriteLine("Accès réseau...");
+
+            try
+            {
+                //Téléchargement image synchrone
+                webClient.DownloadFile(url, "papillon.jpg");
+                Console.WriteLine("Téléchargement terminé");
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response != null)
+                {
+                    var statusCode = ((HttpWebResponse)ex.Response).StatusCode;
+                    if (statusCode == HttpStatusCode.NotFound)
+                    {
+                        //Erreur accès fichier
+                        Console.WriteLine("ERREUR RESEAU : Non trouvé");
+                    }
+                    else
+                    {
+                        //Erreur accès serveur
+                        Console.WriteLine("ERREUR RESEAU : " + statusCode);
+                    }
+                }
+                else
+                {
+                    //Erreur syntaxe
+                    Console.WriteLine("ERREUR RESEAU : " + ex.Message);
+                }
+
+            }
+        }
+    }
+}
+
