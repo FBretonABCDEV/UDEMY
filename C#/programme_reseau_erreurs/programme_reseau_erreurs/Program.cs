c045c923714834a35c10da12cee09ccfc2491567
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-08-29 10:32:35 +0200

    cas erreurs

---
+++ C#/programme_reseau_erreurs/programme_reseau_erreurs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+
+namespace programme_reseau_erreurs
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://codeavec_jonathan.com/res/pizzass1.json";
+
+            var webClient = new WebClient();
+
+            Console.WriteLine("Accès réseau...");
+
+            try
+            {
+                string reponse = webClient.DownloadString(url);
+                Console.WriteLine(reponse);
+            }
+            catch(WebException ex)
+            {
+                if(ex.Response != null)
+                {
+                    var statusCode = ((HttpWebResponse)ex.Response).StatusCode;
+                    if (statusCode == HttpStatusCode.NotFound)
+                    {
+                        //Erreur accès fichier
+                        Console.WriteLine("ERREUR RESEAU : Non trouvé");
+                    }
+                    else
+                    {
+                        //Erreur accès serveur
+                        Console.WriteLine("ERREUR RESEAU : " + statusCode);
+                    }
+                }
+                else
+                {
+                    //Erreur syntaxe
+                    Console.WriteLine("ERREUR RESEAU : " + ex.Message);
+                }
+                
+            }   
+        }
+    }
+}
