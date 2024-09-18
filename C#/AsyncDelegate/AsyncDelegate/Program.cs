0331d52c733796613a3527abf01ef52eb88bd5ca
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-09-18 18:14:07 +0200

    modification gitignore

---
+++ C#/AsyncDelegate/AsyncDelegate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace AsyncDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fonction Asynchrone ou Call Back
+            var webClient = new WebClient();
+        }
+    }
+}
