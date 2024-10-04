424240a64a1ae0ea9c783bed91e449dc61ea478a
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-10-05 00:00:44 +0200

    Création InitOnlyProperties

---
+++ C#/Nouveautes_cs9/Nouveautes_cs9/InitOnlyProperties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InitOnlyProperties
+{
+    internal class Program
+    {
+        class Personne
+        {
+            // Assigner la valeur à la construction du projet
+            public string nom { get; init; }
+            public int age { get; set; }
+
+            public void Afficher()
+            {
+                Console.WriteLine("nom : " + nom + " - age : " + age + " ans");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var personne1 = new Personne() { nom = "toto", age = 20 }; // Pas obligé de créer un constructeur, on peut garder cette syntaxe
+            // personne1.nom = "tata";
+            personne1.Afficher();
+            Console.WriteLine("nom de la personne1 : " + personne1.nom);
+        }
+    }
+}
---
+++ C#/Nouveautes_cs9/Nouveautes_cs9/TopLevelPrograms/Program.cs
@@ -0,0 +1,4 @@
+﻿// Top level programs les using, namespace ainsi que le point d'entrée main sont masqués.
+// On peut entrer directement le code il sera dans la fonction main
+System.Console.WriteLine("Hello World!");
+
